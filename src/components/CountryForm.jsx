bb177d674054f61f570bc1fb54bb5650d7c95f25
juanandma <juanandma@disroot.org>
2023-10-27 17:07:48 +0200

    CountryForm tested. TODO e2e y desacoplamiento

--- src/components/CountryForm.jsx
+++ src/components/CountryForm.jsx
@@ -1,42 +1,99 @@
-import { FieldSet } from "./FieldSet.jsx";
-import { Field } from "./Field.jsx";
-import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 export const CountryForm = ({ addCountry }) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const [ip, setIp] = useState("");
+  const [errors, setErrors] = useState({
+    ip: "",
+  });
 
-  const submitForm = async (formData) => {
-    console.log(formData);
-    //await addCountry(formData)
+  const handleSubmit = async (event) => {    
+    event.preventDefault();
+    console.log("Form ip: ", ip);
+    
+    if (!isValid()) return;
+    //Pasar al dominio?    
+    setIp("");
+    console.log("Form ip valida");
+    //try catch
+    await addCountry(ip);
+    //finally    
   };
 
+  function handleChange(event) {
+    const ipActualizada = event.target.value;
+
+    console.log(ipActualizada)
+    
+    //actualiza ip. Solo existe un campo.
+    setIp(ipActualizada);
+    //Error si la ip no es valida
+    setErrors(() => validate(ipActualizada));   
+  }
+
+  //Pasar al dominio?
+  function validate(ip) {
+    let errors = { ip:  ""};
+    if (ip.length === 0) {
+      errors.ip = "Ip Address is required";
+    }
+    if (ip.length > 0 && ip.length < 7) {
+      errors.ip = "Ip Address needs to be at least 7 characters.";
+    }
+    /*
+    if (check_times_char(".")) {
+      errors.ip = "Ip Address needs to contain 4 dots";
+    }
+    */
+    return errors;
+  }
+
+  function isValid() {
+    return (
+      errors.ip.length === 0
+    );
+  }
+
+  //testeo
+  function check_times_char(str1, char, times) {
+    /*
+    let cont = 0;
+    let overlimit = false;
+    let i = 0;
+    while (i < str1.length && !overlimit) {
+      if (str1.charAt(i) == char && cont <= times) {
+        cont++;
+        if (cont <= times) {
+          overlimit = true;
+        }
+        i++;
+      }
+    }
+    if (cont = times && !overlimit) return true;
+    return false;
+    */
+    return true
+  }
+
   return (
-    <div>
-      <h1>New Country Form</h1>
-      <form onSubmit={handleSubmit(submitForm)}>
-        <FieldSet label="CountryDefinition">
-          <Field label="Name" error={errors.name}>
-            <input
-              {...register("name", { required: "This field is required" })}
-              type="text"
-              id="name"
-            />
-          </Field>
-          <Field label="IpAdress" error={errors.ipadress}>
-            <input
-              {...register("ipadress", { required: "This field is required" })}
-              type="text"
-              id="ipadress"
-            />
-          </Field>
-        </FieldSet>
-        <Field>
-          <button type="submit">Save</button>
-        </Field>
+    <div className="country-container">
+      <h1>Add new country</h1>
+      <form className="country-form" onSubmit={handleSubmit}>
+        <label htmlFor="ip">Ip Adress</label>
+        <input
+          id="ip"
+          value={ip}
+          onChange={handleChange}
+          placeholder="Enter the ip address of the new country"
+          required
+          type="text"
+          name="ip"
+        ></input>
+        {errors.ip.length > 0 && (
+          <div role="alert" className="ip error">
+            <p>{errors.ip}</p>
+          </div>
+        )}
+        <button type="submit">Save</button>
       </form>
     </div>
   );
--- src/components/FieldSet.jsx
+++
@@ -1,9 +0,0 @@
-
-export const FieldSet = ({ label, children }) => {
-  return (
-    <div>
-      {label && <legend>{label}</legend>}
-      <div>{children}</div>
-    </div>
-  );
-};
