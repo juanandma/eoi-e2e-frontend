7ef0c845e061b40cd315356339e27387080ea2be
Dani Tallón <kalimero99@protonmail.ch>
2023-10-27 16:31:39 +0200

    delete unused tests, fix styles and add new components to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
+import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeView from "./views/HomeView.jsx";
 import CountryView from "./views/CountryView.jsx";
-import { NewCountryForm } from "./components/NewCountryForm";
+import FormView from "./views/FormView.jsx";
+import Header from "./components/Header";
 
 export const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<HomeView />} />
           <Route path="/list" element={<CountryView />} />
-          <Route path="/form" element={<NewCountryForm />} />
+          <Route path="/form" element={<FormView />} />
         </Routes>
       </BrowserRouter>
     </div>
